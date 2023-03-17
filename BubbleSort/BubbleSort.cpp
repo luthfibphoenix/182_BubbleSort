79b6f845ee166fdd9e3217b2f7d4ad7b90a523eb
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-03-17 11:11:46 +0700

    menambahkan keterangan

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main()
-int a[20];
-int n;
+int a[20];				// Deklarasi array a dengan ukuran 20		
+int n;					// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+
+void input() {
+	while (true) {
+		cout << "masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\";
+
+		}
+	}
+}
